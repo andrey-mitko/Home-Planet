aac1a530cbe4be580fa7f9f3bf4973dc829c15bb
Andrey Mitko <andrey@crowdhaus.co.uk>
2022-07-19 10:33:26 +0100

    Added hands animations

--- home-planet.js
+++ home-planet.js
@@ -1,7 +1,7 @@
 const eyesTimeline = gsap.timeline({
     repeat: -1,
 })
-const eyeballs = document.querySelectorAll('path#ball, path#ball_2, path#ball_3, path#ball_4, path#ball_5')
+const eyeballs = document.querySelectorAll('path#ball, path#ball_2, path#ball_3, path#ball_4, path#ball_5, path#ball_6')
 eyesTimeline
     .set(eyeballs, {
         y: 0,
@@ -40,4 +40,43 @@ hatTimeline
         duration: 0.5,
         delay: 0.1,
     })
-hat
+
+
+const leftArmTimeline = gsap.timeline({
+    repeat: -1,
+})
+const rightArmTimeline = gsap.timeline({
+    repeat: -1,
+})
+const leftArm = document.querySelector('#left-arm');
+const rightArm = document.querySelector('#right-arm');
+
+leftArmTimeline
+    .set(leftArm, {
+        rotation: 0,
+    })
+    .to(leftArm, {
+        rotation: 20,
+        duration: 0.25,
+        delay: 2
+    })
+    .to(leftArm, {
+        rotation: 0,
+        delay: 2,
+        duration: 0.25,
+    })
+rightArmTimeline
+    .set(rightArm, {
+        rotation: 0,
+    })
+    .to(rightArm, {
+        rotation: -20,
+        duration: 0.25,
+        delay: 3
+    })
+    .to(rightArm, {
+        rotation: 0,
+        delay: 2,
+        duration: 0.25,
+    })
+
